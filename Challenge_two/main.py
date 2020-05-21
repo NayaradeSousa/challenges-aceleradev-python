93e22e851aa0ee2c1bf07bcfaaeed69fdad475d1
NayaradeSousa <naydisousa@gmail.com>
2020-05-21 19:07:58 +0200

    Code improvements

--- Challenge_two/main.py
+++ Challenge_two/main.py
@@ -1,6 +1,6 @@
 """
 Code refactoring for improve security, standard and good practices in case of company expansion.
-
+################################ Challenge Aceleradev Python ##########################################################
 - Protect the Employee class from being instantiated directly.
 - Make the implementation of the methods of the Employee class mandatory, implement them if necessary.
 - Protect the department attribute of the Manager class so that it is accessed only through the get_department method.
@@ -12,7 +12,7 @@ sales are cumulative
 department name for the Manager and Seller classes
 - Standardize an 8-hour workload for all employees.
 The calculation of the Seller's calc_bonus method must be calculated by the total sales times 0.15
-
+#######################################################################################################################
 """
 # This library is to make the Employee class abstract.
 from abc import ABC, abstractmethod
@@ -24,13 +24,14 @@ class Department:
         self.code = code
 
 
+# Abstract class - this prevent directly access
 class Employee(ABC):
     def __init__(self, code, name, salary, department):
         self.code = code
         self.name = name
         self.salary = salary
+        self.__department = department
         self.hours = 8
-        self.__departament = Department('managers', 1)
 
     @abstractmethod
     def calc_bonus(self):
@@ -41,16 +42,15 @@ class Employee(ABC):
         return self.hours
 
     def get_departament(self):
-        return self.__departament.name
+        return self.__department.name
 
-    def set_departament(self, department):
-        self.__departament.name = department
+    def set_departament(self, new_depart):
+        self.__department.name = new_depart
 
 
 class Manager(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.set_departament(Department('managers', 1))
+        super().__init__(code, name, salary, Department('managers', 1))
 
     def calc_bonus(self):
         return self.salary * 0.15
@@ -61,8 +61,7 @@ class Manager(Employee):
 
 class Seller(Employee):
     def __init__(self, code, name, salary):
-        super().__init__(code, name, salary)
-        self.set_departament(Department('sellers', 2))
+        super().__init__(code, name, salary, Department('sellers', 2))
         self.__sales = 0
 
     def get_hours(self):
@@ -86,9 +85,9 @@ if __name__ == '__main__':
     leandro = Manager(1011, 'Leandro', 5000)
     paulo = Seller(1213, 'Paulo', 4500)
     print("****************************************************************")
-    print(f"Your department is {maria.get_departament()}")
-    print(f"Your department is {leandro.get_departament()}")
-    print(f"Your department is {paulo.get_departament()}")
+    print(f"Your department is {maria.get_departament()}.")
+    print(f"Your department is {leandro.get_departament()}.")
+    print(f"Your department is {paulo.get_departament()}.")
     print("****************************************************************")
     print(f"{maria.name}, you must work {maria.get_hours()} hours by day.")
     print(f"{leandro.name}, you must work {leandro.get_hours()} hours by day.")
@@ -97,7 +96,7 @@ if __name__ == '__main__':
     paulo.put_sales(900)
     paulo.put_sales(10)
 
-    print(f"{maria.name} your salary is: {maria.salary} and your bonus this month is R${maria.calc_bonus()}")
-    print(f"{leandro.name} your salary is: {leandro.salary} and your bonus this month is R${leandro.calc_bonus()}")
-    print(f"{paulo.name} your salary is: {paulo.salary} and your bonus this month is R${paulo.calc_bonus()}")
+    print(f"{maria.name} your salary is: {maria.salary} and your bonus this month is R${maria.calc_bonus()}.")
+    print(f"{leandro.name} your salary is: {leandro.salary} and your bonus this month is R${leandro.calc_bonus()}.")
+    print(f"{paulo.name} your salary is: {paulo.salary} and your bonus this month is R${paulo.calc_bonus()}.")
     print("****************************************************************")
